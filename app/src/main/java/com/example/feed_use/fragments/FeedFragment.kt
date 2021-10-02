e1c880edb3f7d7644efb83938d23156caba4e4e3
Pedro <pbruno1283@gmail.com>
2021-10-02 14:03:14 -0300

    Divider para os posts criado

--- app/src/main/java/com/example/feed_use/fragments/FeedFragment.kt
+++ app/src/main/java/com/example/feed_use/fragments/FeedFragment.kt
@@ -5,29 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.content.res.AppCompatResources.getDrawable
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.feed_use.DataProvider
 import com.example.feed_use.R
+import com.example.feed_use.adapters.AdapterPost
+import com.example.feed_use.databinding.FragmentFeedBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [FeedFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FeedFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var adapterPost: AdapterPost
+    private lateinit var binding:FragmentFeedBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -35,26 +27,23 @@ class FeedFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_feed, container, false)
+        binding = FragmentFeedBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FeedFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FeedFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+       setRecyclerView()
+    }
+
+    private fun setRecyclerView(){
+        val layout = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+        adapterPost = AdapterPost(DataProvider.postList)
+        binding.recyclerViewPosts.apply {
+            layoutManager = layout
+            adapter = adapterPost
+            val itemDecoration = DividerItemDecoration(context,DividerItemDecoration.VERTICAL)
+            itemDecoration.setDrawable(getDrawable(context,R.drawable.divider_post)!!)
+            addItemDecoration(itemDecoration)
+        }
     }
 }
